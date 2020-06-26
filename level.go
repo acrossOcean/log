67429c827408cf68ab5f76f40e610b331beb8357
Guo <guoyueyang@126.com>
2020-06-26 19:13:34 +0800

    给 level 添加 stringer 接口, 提高输出可读性

--- level.go
+++ level.go
@@ -1,11 +1,34 @@
 package log
 
+import "strconv"
+
 type Level int
 
 const (
-	LDebug Level = iota
+	_ Level = iota
+	LDebug
 	LInfo
 	LWarn
 	LError
 	LPanic
 )
+
+func (receiver Level) String() string {
+	str := ""
+	switch receiver {
+	case LDebug:
+		str = "debug"
+	case LInfo:
+		str = "info"
+	case LWarn:
+		str = "warn"
+	case LError:
+		str = "error"
+	case LPanic:
+		str = "panic"
+	default:
+		str = "unknown:" + strconv.Itoa(int(receiver))
+	}
+
+	return str
+}
